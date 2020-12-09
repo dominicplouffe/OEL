78180e3b9b07f5409c82569e8a0d34794b27b1bb
Dominic Plouffe <dominic@dplouffe.ca>
2020-12-09 08:00:18 -0500

    updated deploy script to run migrations

--- deploy.py
+++ deploy.py
@@ -63,6 +63,7 @@ def copy_code(ssh):
     ssh.exec_command('sudo cp -r tasks /opt/oel')
     ssh.exec_command('sudo cp manage.py /opt/oel')
     ssh.exec_command('sudo cp requirements.txt /opt/oel')
+    ssh.exec_command('PYTHONPATH=/opt/oel python3 /opt/oel/manage.py migrate')
     ssh.exec_command('sudo systemctl restart oel')
     ssh.exec_command('sudo systemctl restart oelsup')
 
