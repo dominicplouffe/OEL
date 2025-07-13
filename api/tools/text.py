a35adf56f486c5fe4ee5708372472acbf9ed3f3a
Dominic Plouffe <dominic@dplouffe.ca>
2025-07-12 21:07:09 -0400

    updated the text tool

--- api/tools/text.py
+++ api/tools/text.py
@@ -2,29 +2,23 @@ from oel.settings import TWILIO_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE
 from twilio.rest import Client
 
 
-def sent_text_message(to, body):
+def send_text_message(to, body):
     client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
-    client.messages.create(
-        from_=TWILIO_PHONE,
-        to=to,
-        body=body
-    )
+    client.messages.create(from_=TWILIO_PHONE, to=to, body=body)
 
 
 def send_going_offcall(phone_number):
-
-    sent_text_message(
+    send_text_message(
         phone_number,
         "OnErrorLog: Just letting you know that your on-call assistance"
-        " is no longer needed"
+        " is no longer needed",
     )
 
 
 def send_going_oncall(phone_number):
-    sent_text_message(
-        phone_number,
-        "OnErrorLog: Just letting you know that you are going on call"
+    send_text_message(
+        phone_number, "OnErrorLog: Just letting you know that you are going on call"
     )
 
 
@@ -32,10 +26,7 @@ def send_ping_success(phone_number, ping_name):
 
     body = "OnErrorLog Success : %s - " % ping_name
     body += "Your ping has been recovered, everything is back to normal."
-    sent_text_message(
-        phone_number,
-        body
-    )
+    send_text_message(phone_number, body)
 
 
 def send_failure(phone_number, ping_name, doc_link, fail_res):
@@ -44,10 +35,7 @@ def send_failure(phone_number, ping_name, doc_link, fail_res):
     body += " enpoint. Please login to onErrorLog and check it out."
     body += " - https://app.onerrorlog.com/failure/%s/" % fail_res.id
 
-    if doc_link and doc_link.startswith('http'):
+    if doc_link and doc_link.startswith("http"):
         body += " - Documentation: %s" % doc_link
 
-    sent_text_message(
-        phone_number,
-        body
-    )
+    send_text_message(phone_number, body)
